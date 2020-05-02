cc1be3d8c46ecc2be984178c2f2dc90391c8a76c
motaouia <med.motaouia@gmail.com>
2020-05-03 00:54:49 +0200

    Login Authentication I

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyD0qhYfCD_egk_8lJgc4yyoC3cAsF-au9I",
+    apiKey: "XXXXXX",
     authDomain: "clientpanelprod-a63fa.firebaseapp.com",
     databaseURL: "https://clientpanelprod-a63fa.firebaseio.com",
     projectId: "clientpanelprod-a63fa",
     storageBucket: "clientpanelprod-a63fa.appspot.com",
     messagingSenderId: "770132496134",
-    appId: "1:770132496134:web:fdc4b256150e24c5239887",
-    measurementId: "G-Z9FELTD6GV"
+    appId: "XXXXX",
+    measurementId: "XXXXX"
   }
 };
